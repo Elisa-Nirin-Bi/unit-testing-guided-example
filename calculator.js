d9420c874b5364ea8445c369aa37f100033e416e
Elisa Barontini <nirinproductions@gmail.com>
2021-06-12 10:30:31 +0100

    Operations done

--- calculator.js
+++ calculator.js
@@ -1,13 +1,40 @@
 function sum(a, b) {
-  return;
-}
+  if (typeof a === 'number' && b === undefined){
+    return a + 0;
+  }else if (a === undefined ){
+    return 0;
+  } else {
+  return a + b;
+}}
 
 function subtract(a, b) {
-  return;
-}
+  if( typeof a ===  "number" && b === undefined) {
+    return a - 0 ;
+  }else if( a === undefined && b === undefined){
+    return 0
+  }else if( a === undefined && typeof b ===  "number" ){
+    return 0 - b ;
+  }else if(parseFloat(a) && parseFloat(b)){
+    return parseFloat(a - b);
+  }else if (parseFloat(a) || parseFloat(b)){
+    return parseFloat(a - b);
+  }else{
+    return a - b
+  }
+    
+  }
+
 
 function divide(a, b) {
-  return;
+ if(typeof a ===  "number" && b === 0 ){
+  throw ("You cannot divide by 0");
+}else if(parseFloat(a) && parseFloat(b)){
+    return parseFloat(a /  b);
+  }else if (parseFloat(a) || parseFloat(b)){
+    return parseFloat(a / b); }
+ else{
+  return a / b;
+  }
 }
 
 function multiply(a, b) {
